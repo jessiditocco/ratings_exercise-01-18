4ab82c7301cf2090ddc88bdcc09f6e232b3c73ff
Hackbright Student <no-reply@hackbrightacademy.com>
2018-02-01 12:55:31 -0800

    add pearson and similarity methods

--- model.py
+++ model.py
@@ -2,6 +2,8 @@
 
 from flask_sqlalchemy import SQLAlchemy
 
+from correlation import pearson
+
 # This is the connection to the PostgreSQL database; we're getting this through
 # the Flask-SQLAlchemy helper library. On this, we can find the `session`
 # object, where we do most of our interactions (like committing, etc.)
@@ -29,6 +31,26 @@ class User(db.Model):
         """Provide helpful representation when printed."""
 
         return "<User user_id={} email={}>".format(self.user_id, self.email)
+
+    def similarity(self, other):
+        """Return Pearson rating for user compared to other user."""
+
+        u_ratings = {}
+        paired_ratings = []
+
+        for r in self.ratings:
+            u_ratings[r.movie_id] = r
+
+        for r in other.ratings:
+            u_r = u_ratings.get(r.movie_id)
+            if u_r:
+                paired_ratings.append((u_r.score, r.score))
+
+        if paired_ratings:
+            return pearson(paired_ratings)
+
+        else:
+            return 0.0
 
 
 # Put your Movie and Rating model classes here.
